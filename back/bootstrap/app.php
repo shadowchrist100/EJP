960ad968843ad30d8f205cfd07ed1879798be311
Abel <lewheabel16@gmail.com>
2026-03-12 15:41:59 +0100

    cors resolving

--- back/bootstrap/app.php
+++ back/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,7 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
-        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+        $middleware->prepend(HandleCors::class);  
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
