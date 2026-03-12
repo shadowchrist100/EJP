ff2c17ede751dfe5959b62f4748f41ea1863051b
Abel <lewheabel16@gmail.com>
2026-03-12 15:56:36 +0100

    cors resolving

--- back/bootstrap/app.php
+++ back/bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\HandleCors;
 use App\Http\Middleware\Cors;
 
 return Application::configure(basePath: dirname(__DIR__))
